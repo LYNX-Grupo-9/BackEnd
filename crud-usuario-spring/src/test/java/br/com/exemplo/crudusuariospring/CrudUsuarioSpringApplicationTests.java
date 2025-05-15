775e44ef7781590a14d8510c48a04e8aaa38f9fa
Lucas-Ronald <lucas.silva829@sptech.school>
2025-05-14 23:48:50 -0300

    Ajustes pom XML e properties

--- crud-usuario-spring/src/test/java/br/com/exemplo/crudusuariospring/CrudUsuarioSpringApplicationTests.java
+++ crud-usuario-spring/src/test/java/br/com/exemplo/crudusuariospring/CrudUsuarioSpringApplicationTests.java
@@ -1,9 +1,12 @@
 package br.com.exemplo.crudusuariospring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrudUsuarioSpringApplicationTests {
 
 	@Test
